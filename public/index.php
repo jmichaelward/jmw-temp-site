e4a2fd008c02dd92372ce109d4e180095ed006e7
Jeremy Ward <jeremy@jmichaelward.com>
2024-10-09 19:13:39 -0500

    v1.0 of this very temporary site. (#1)

---
+++ public/index.php
@@ -0,0 +1,92 @@
+<html lang="en-US">
+<head>
+    <title>J. Michael Ward - Software Engineer and Board Game Enthusiast</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1" />
+    <meta name="description" content="Software Engineer and Board Game Enthusiast" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="J. Michael Ward" />
+    <meta property="og:description" content="Software Engineer and Board Game Enthusiast" />
+    <meta property="og:url" content="https://jmichaelward.com/" />
+    <meta property="og:site_name" content="J. Michael Ward" />
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css" />
+    <link rel="shortcut icon" href="assets/favicon.ico" />
+    <link rel="canonical" href="https://jmichaelward.com/" />
+    <link rel="me" href="https://phpc.social/@jmichaelward" />
+</head>
+<body>
+    <div class="site-container">
+        <header class="site-header"></header>
+        <div class="site-container__inner">
+            <main class="site-main">
+                <article class="post">
+                    <header class="entry__hdr">
+                        <h1 class="entry__hd">Moving Forward!</h1>
+                        <div class="entry__meta">October 9, 2024</div>
+                    </header>
+                    <div class="entry__content-container">
+                        <div class="entry__content">
+                            <p>
+                                <a href="https://www.theverge.com/2024/10/4/24262232/matt-mullenweg-wordpress-org-wp-engine">It's time to move away from WordPress.</a>
+                            </p>
+
+                            <p>This blog and much of my web development career has been "proudly powered by WordPress" for
+                            the last decade and then some, but last spring I completed my final integration project on
+                            the platform. I've been happily building code-based solutions in other platforms and frameworks
+                            since. In the interim, I had been keeping up with the latest updates to this site, but hadn't actively
+                            pursued rebuilding it using something else because, frankly, I just haven't felt up to it.</p>
+
+                            <p>The recent legal battles between WP Engine and Automattic, and in particular, what I feel are antagonistic
+                                communications and actions which threaten the safety and stability of the WordPress community, give me the
+                                impetus I need to finally port my data and build a new home for myself.</p>
+
+                            <p>For now, it means this static placeholder site written in good-old HTML and CSS, but longer-term, it means
+                                tinkering with something new – perhaps building upon my growing knowledge around Laravel, adopting a smaller but
+                                similarly-storied CMS like Drupal, or perhaps even exploring a completely different language altogether.
+                                I don't know what the next iteration of my personal site will bring, but I'm looking forward to trying
+                                something new and moving away from the chaos and drama. It's literally all I want for 2025, in every aspect
+                                of my life.
+                            </p>
+
+                            <p>Here's to new adventures with code. I hope to be back soon writing about all the fun and interesting challenges
+                                I've been facing in my last year and a half with Laravel.</p>
+                        </div>
+                    </div>
+                </article>
+            </main>
+            <div class="sidebar-container sidebar-container--main">
+                <aside class="sidebar sidebar--main">
+                    <div class="sidebar__image">
+                        <img alt="Portrait image of J. Michael Ward" src="./assets/images/jmw-profile-image.jpeg" loading="lazy" decoding="async" />
+                    </div>
+                    <div class="sidebar__details">
+                        <p class="sidebar-details__title">
+                            <a href="https://jmichaelward.com" title="J. Michael Ward"><span>J. Michael Ward</span></a>
+                        </p>
+                        <p class="sidebar-details__description">Software Engineer and Board Game Enthusiast</p>
+                    </div>
+                </aside>
+            </div>
+            <div class="sidebar-container sidebar-container--supplemental">
+                <aside class="sidebar sidebar--links">
+                    <h2>Elsewhere Online</h2>
+                    <div class="menu-external-links-container">
+                        <ul id="menu-external-links" class="menu">
+                            <li class="menu-item"><a title="J. Michael Ward on GitHub" href="https://github.com/jmichaelward">GitHub</a></li>
+                            <li class="menu-item"><a title="Jeremy Ward on LinkedIn" href="https://linkedin.com/in/jeremyward">LinkedIn</a></li>
+                            <li class="menu-item"><a title="J. Michael Ward on Mastodon" href="https://phpc.social/@jmichaelward">Mastodon</a></li>
+                        </ul>
+                    </div>
+                </aside>
+            </div>
+        </div><!-- .site-container__inner -->
+        <footer class="site-footer">
+            <div class="site-footer__inner">
+                <p class="site-footer__copyright">&copy;2009-<?php echo date('Y'); ?> J. Michael Ward</p>
+            </div>
+        </footer>
+    </div><!-- .site-container-->
+</body>
+</html>
